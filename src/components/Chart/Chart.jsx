332372af4b1439d8ccd3f7655f3e9d387a7ac299
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 17:30:52 +0530

    This commit is made by Auto-Git 474, Tuesday 09 March 2021 09:19:08 AM IST

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from 'react';
-
+import {fetchDailyData} from '../../api';
 const Chart = () => {
     const [dailyData, setDailyData] = useState({});
+
+    useEffect( () =>{
+        const fetchDailyData
+    });
     return (
         <h1>Charts</h1>
     )
