4619bffb313053ffc354a7a754ee9aaa322fa358
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 17:20:22 +0530

    This commit is made by Auto-Git 464, Tuesday 09 March 2021 09:19:08 AM IST

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid } from '@material-ui/core';
-import styles from 'Cards.module.css';
+import styles from './Cards.module.css';
 import Countup from 'react-countup';
 import cx from 'classnames';
 
