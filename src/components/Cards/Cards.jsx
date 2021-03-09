617b9b2c0f25a2800acd5bd78888468608991861
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 09:37:39 +0530

    This commit is made by Auto-Git 19, Tuesday 09 March 2021 09:19:08 AM IST

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid } from '@material-ui/core';
-
+import styles from './Cards.module.css'
 const Cards = (bruh) => {
     console.log(bruh);
     return (
-        <h1>Cards</h1>
+        <div className = {styles.container}>
+            <Grid container spacing={3} justify="center">
+
+            </Grid>
+        </div>
     )
 }
 
