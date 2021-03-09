18058789ca6f21d14c409242831b298e20130bf5
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 23:28:35 +0530

    This commit is made by Auto-Git 152, Tuesday 09 March 2021 06:20:06 PM IST

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     state = {
         // Populating the fetched data to data{}
         data: {},
+        country: '',
     }
 
     // Data fetched from the API here
