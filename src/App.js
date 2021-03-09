dfbcca92ec06ada2812144ba22284ace5b58285e
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 23:46:57 +0530

    This commit is made by Auto-Git 161, Tuesday 09 March 2021 06:20:06 PM IST

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
 
     handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
+        console.log(fetchedData);
     }
 
     render() {
