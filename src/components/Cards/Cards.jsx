63f406e2f3b94b775a8ced2d079aafbcc5bee203
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 09:57:25 +0530

    This commit is made by Auto-Git 38, Tuesday 09 March 2021 09:19:08 AM IST

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid } from '@material-ui/core';
 import styles from './Cards.module.css'
+
 const Cards = ({data :{confirmed, recovered, deaths, lastUpdate}}) => {
     if (!confirmed) {
         return "Fetching Data...";
