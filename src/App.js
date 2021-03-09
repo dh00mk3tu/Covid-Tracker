da55b598d3d7f0515952c23998399d34f319adeb
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 09:19:09 +0530

    This commit is made by Auto-Git 1, Tuesday 09 March 2021 09:19:08 AM IST

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
 
     async componentDidMount() {
         const data = await fetchData();
+        console.log(data);
     }
 
     render() {
--- src/api/index.js
+++ src/api/index.js
@@ -5,10 +5,10 @@ const url = 'https://covid19.mathdro.id/api';
 // get requests 
 export const fetchData = async() => {
     try {
-        const response = await axios.get(url);
-        
-        // Test Code
-        return response;
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+
+        return {confirmed, recovered, deaths, lastUpdate};
+
         // console.log(response);
     }
     catch (error) {
