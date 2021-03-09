912dcde95ae9bdf3803986ccbc5051d1dabb3c64
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 17:04:34 +0530

    This commit is made by Auto-Git 449, Tuesday 09 March 2021 09:19:08 AM IST

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid } from '@material-ui/core';
 import styles from './Cards.module.css'
-import Countup from './Countup.'
+import Countup from 'react-countup';
 
 const Cards = ({data :{confirmed, recovered, deaths, lastUpdate}}) => {
     // console.log(confirmed);
