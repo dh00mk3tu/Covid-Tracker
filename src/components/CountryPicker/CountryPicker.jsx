4691154ec75ea9342d16cf260cdf9ad3ec25aa7d
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 23:24:25 +0530

    This commit is made by Auto-Git 150, Tuesday 09 March 2021 06:20:06 PM IST

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -2,17 +2,18 @@
 import React, {useState, useEffect} from 'react';
 import {NativeSelect, FormControl} from '@material-ui/core';
 import styles from './CountryPicker.module.css';
-import { countries } from '../../api';
+import { fetchCountries } from '../../api';
 
 const CountryPicker = () => {
 
     const [fetchedCountries, setFetchedCountries] = useState([]);
     useEffect(() => {
-        const fetchCountries = async() => {
-            setFetchedCountries(await countries);
+        const fetchAPI = async() => {
+            setFetchedCountries(await fetchCountries());
         }
-        fetchCountries();
+        fetchAPI();
     }, [setFetchedCountries]);
+
     return (
         <FormControl className={styles.formControl}>
             <NativeSelect>
