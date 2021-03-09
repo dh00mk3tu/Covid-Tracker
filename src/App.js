63f972ec957f367dd012c2ad749c82dd670dd953
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 08:52:31 +0530

    This commit is made by Auto-Git 7, Tuesday 09 March 2021 08:46:15 AM IST

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 // Importing Components
+// Cards Component
+// import Cards from './components/Cards/Cards';
+
+
+import {
+    Cards, Chart, CountryPicker } from './components';
+}
 class App extends React.Component {
     render() {
         return (
