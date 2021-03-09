10caed90327a8d46900a6a1d42e560e8462b3a99
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-10 01:55:13 +0530

    This commit is made by Auto-Git 1, Wednesday 10 March 2021 01:55:12 AM IST

--- src/api/index.js
+++ src/api/index.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async (country) => {
-  let changeableUrl = url;
+  let mutUrl = url;
 
   if (country) {
-    changeableUrl = `${url}/countries/${country}`;
+    mutUrl = `${url}/countries/${country}`;
   }
 
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(mutUrl);
 
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
