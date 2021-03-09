8366d8ac09526c28afa3797b1fa943822cd1d3f8
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-09 23:57:07 +0530

    This commit is made by Auto-Git 166, Tuesday 09 March 2021 06:20:06 PM IST

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -13,7 +13,7 @@ const CountryPicker = ({handleCountryChange}) => {
         }
         fetchAPI();
     }, [setFetchedCountries]);
-    console.log(fetchedCountries);
+    console.log("breuh",fetchedCountries);
     return (
         <FormControl className={styles.formControl}>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
