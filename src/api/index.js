3daf7d8e1a5b8ae714f6bb791aa9ccdac410e827
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-10 14:04:17 +0530

    fix for netlify build

--- src/api/index.js
+++ src/api/index.js
@@ -42,7 +42,6 @@ export const fetchDailyData = async () => {
 export const fetchCountries = async () => {
   try {
     const { data: { countries } } = await axios.get(`${url}/countries`);
-
     return countries.map((country) => country.name);
   } catch (error) {
     return error;
